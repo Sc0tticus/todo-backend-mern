1e6c26306c16c9ac94313d56af82d078c0b18dfa
Sc0tticus <ssinger303@gmail.com>
2023-06-11 11:46:15 -0600

    Refactored and created routes directory

---
+++ routes/routes.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const Todo = require('../models/Todo');
+
+// The GET / todos route uses the Todo.find() method.
+// This is an asynchronous operation that returns a Promise.
+// The reason for this is that it has to read data from the MongoDB database,
+// which is an I / O operation that can take an indeterminate amount of time.
+// Thus, to avoid blocking the rest of the code from running while it waits for the database operation, it's done asynchronously.
+// The await keyword is used to pause execution of the function until the Promise resolves,
+// 	and then the result of the Promise(the todos from the database) is assigned to the todos variable.
+router.get('/todos', async (req, res) => {
+	const todos = await Todo.find();
+
+	res.json(todos);
+});
+
+router.post('/todo/new', async (req, res) => {
+	const todo = new Todo({
+		text: req.body.text
+	});
+
+	try {
+		await todo.save();
+		res.json(todo);
+	} catch (error) {
+		res.status(500).send(error);
+	}
+});
+
+router.delete('/todo/delete/:id', async (req, res) => {
+	const result = await Todo.findByIdAndDelete(req.params.id);
+
+	res.json(result);
+});
+
+router.put('/todo/complete/:id', async (req, res) => {
+	const todo = await Todo.findById(req.params.id);
+
+	todo.complete = !todo.complete;
+
+	todo.save();
+
+	res.json();
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const todoRoutes = require('./routes/routes');
 
 const app = express();
 
@@ -15,38 +16,6 @@ mongoose
 	.then(() => console.log('Connected to DB'))
 	.catch(console.error);
 
-const Todo = require('./models/Todo');
-
-app.get('/todos', async (req, res) => {
-	const todos = await Todo.find();
-
-	res.json(todos);
-});
-
-app.post('/todo/new', (req, res) => {
-	const todo = new Todo({
-		text: req.body.text
-	});
-
-	todo.save();
-
-	res.json(todo);
-});
-
-app.delete('/todo/delete/:id', async (req, res) => {
-	const result = await Todo.findByIdAndDelete(req.params.id);
-
-	res.json(result);
-});
-
-app.put('/todo/complete/:id', async (req, res) => {
-	const todo = await Todo.findById(req.params.id);
-
-	todo.complete = !todo.complete;
-
-	todo.save();
-
-	res.json(todo);
-});
+app.use('/', todoRoutes);
 
 app.listen(3001, () => console.log('Server started on port 3001'));
